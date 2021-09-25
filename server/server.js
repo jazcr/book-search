952b69c7f864ef4c5f87cf909405cd096537c94d
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-09-24 19:50:55 -0600

    add schemas

--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
 const express = require('express');
-const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+//create port
 const PORT = process.env.PORT || 3001;
+//start our app
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+// graph ql server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+//graph ql
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
