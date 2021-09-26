0778f49eab7e26915e7591f8367ef71d1ef31663
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-09-26 13:10:37 -0600

    creating mutations for front end

--- README.md
+++ README.md
@@ -159,13 +159,13 @@ You'll need to create the following front-end files:
 
 * ✔ `mutations.js`:
 
-	* `LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
+	* ✔`LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
 
-	* `ADD_USER` will execute the `addUser` mutation.
+	* ✔`ADD_USER` will execute the `addUser` mutation.
 
-	* `SAVE_BOOK` will execute the `saveBook` mutation.
+	* ✔`SAVE_BOOK` will execute the `saveBook` mutation.
 
-	* `REMOVE_BOOK` will execute the `removeBook` mutation.
+	* ✔`REMOVE_BOOK` will execute the `removeBook` mutation.
 
 Additionally, you’ll need to complete the following tasks in each of these front-end files:
 
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+import gql from "graphql-tag";
+
+// mutation for user login
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+// mutation for creating new user
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $password: String!, $email: String!) {
+        addUser(username: $username, password: $password, email: $email) {
+            token
+            user {
+            username
+            _id
+            email
+            }
+        }
+    }
+`;
+
+// mutation for saving book
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: SavedBookInput) {
+        saveBook(input: $input) {
+        username
+        _id
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            image
+            link
+            title
+            description
+            }
+        }
+    }
+`;
+
+// mutation for deleting a book
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+        _id
+        username
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}
+`;
