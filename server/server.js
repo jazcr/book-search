3018a60764fefaaf02ea37f1129d2db0d7224377
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-09-26 20:56:32 -0600

    change file directory

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/src/index.html'));
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 
